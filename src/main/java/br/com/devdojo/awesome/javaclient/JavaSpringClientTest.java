5c946aa11532342fc4c007024478a6ddadf6227d
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-03-20 20:32:28 -0300

    Spring Boot Essentials - Aula 13 Rest Template parte 2

--- src/main/java/br/com/devdojo/awesome/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/awesome/javaclient/JavaSpringClientTest.java
@@ -2,10 +2,13 @@ package br.com.devdojo.awesome.javaclient;
 
 import br.com.devdojo.awesome.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class JavaSpringClientTest {
     public static void main(String[] args) {
@@ -22,6 +25,12 @@ public class JavaSpringClientTest {
 
         Student[] students = restTemplate.getForObject("/", Student[].class);
         System.out.println(Arrays.toString(students));
+
+        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Student>>() {
+                });
+
+        System.out.println(exchange.getBody());
 
     }
 }
