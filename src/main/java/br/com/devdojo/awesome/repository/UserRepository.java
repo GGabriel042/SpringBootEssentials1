623541baccdc3b296651523e3aab119146fbf073
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-03-20 17:58:01 -0300

    Spring Boot Essentials - Aula 11 Security Parte 2

---
+++ src/main/java/br/com/devdojo/awesome/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package br.com.devdojo.awesome.repository;
+
+import br.com.devdojo.awesome.model.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    User findByUsername(String username);
+}
