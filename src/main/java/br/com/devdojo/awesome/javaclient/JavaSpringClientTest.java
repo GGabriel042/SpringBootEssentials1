0d3b04b5cce6429dfa4749eb1692dd141ef168a0
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-03-20 21:17:47 -0300

    Spring Boot Essentials - Aula 13 Post part 2

--- src/main/java/br/com/devdojo/awesome/javaclient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/awesome/javaclient/JavaSpringClientTest.java
@@ -1,69 +1,20 @@
 package br.com.devdojo.awesome.javaclient;
 
-import br.com.devdojo.awesome.model.PageableResponse;
 import br.com.devdojo.awesome.model.Student;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class JavaSpringClientTest {
     public static void main(String[] args) {
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/protected/students")
-                .basicAuthorization("toyo", "devdojo")
-                .build();
-
-        RestTemplate restTemplateAdmin = new RestTemplateBuilder()
-                .rootUri("http://localhost:8080/v1/admin/students")
-                .basicAuthorization("toyo", "devdojo")
-                .build();
-
-        Student student = restTemplate.getForObject("/{id}", Student.class, 1);
-        ResponseEntity<Student> forEntity = restTemplate.getForEntity("/{id}", Student.class, 1);
-        System.out.println(student);
-        System.out.println(forEntity);
-        System.out.println(forEntity.getBody());
-
-
-//        Student[] students = restTemplate.getForObject("/", Student[].class);
-//        System.out.println(Arrays.toString(students));
-//
-//        ResponseEntity<List<Student>> exchange = restTemplate.exchange("/", HttpMethod.GET, null,
-//                new ParameterizedTypeReference<List<Student>>() {
-//                });
-//
-//        System.out.println(exchange.getBody());
-
-
-        ResponseEntity<PageableResponse<Student>> exchange = restTemplate.exchange("/?sort=id,desc&sort=name,asc", HttpMethod.GET, null,
-                new ParameterizedTypeReference<PageableResponse<Student>>() {
-                });
-        System.out.println(exchange);
-        System.out.println(exchange.getBody().getContent());
-
-
 
         Student studentPost = new Student();
         studentPost.setName("John Wick");
         studentPost.setEmail("John@pencil.com");
-        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/",
-                HttpMethod.POST, new HttpEntity<>(studentPost, createJSONHeader()), Student.class);
+        JavaClientDao dao = new JavaClientDao();
+        System.out.println(dao.findById(1));
+        List<Student> students = dao.listAll();
+        System.out.println(dao.save(studentPost));
+        System.out.println(students.size());
 
-        Student studentPostForObject = restTemplateAdmin.postForObject("/", studentPost, Student.class);
-        ResponseEntity<Student> studentPostForEntity = restTemplateAdmin.postForEntity("/", studentPost, Student.class);
-
-        System.out.println(exchangePost);
-        System.out.println(studentPostForObject);
-        System.out.println(studentPostForEntity);
-    }
-
-    private static HttpHeaders createJSONHeader() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return headers;
     }
 }
