a711b02b845493d74a8d6f0b5af83a2ef2a8f27e
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-03-24 18:50:42 -0300

    Spring Boot Essentials - Aula 14 Testes Unitários parte 3

---
+++ src/test/java/br/com/devdojo/StudentEndpointTest.java
@@ -0,0 +1,51 @@
+package br.com.devdojo;
+
+import br.com.devdojo.repository.StudentRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class StudentEndpointTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @LocalServerPort
+    private int port;
+    @MockBean
+    private StudentRepository studentRepository;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @TestConfiguration
+    static class Config{
+        @Bean
+        public RestTemplateBuilder restTemplateBuilder() {
+            return new RestTemplateBuilder().basicAuthorization("toyo", "devdojo");
+        }
+    }
+
+
+    @Test
+    public void listStudentsWhenUsernameAndPasswordAreIncorrectShouldReturnStatusCode401() {
+        restTemplate = restTemplate.withBasicAuth("1", "1");
+        ResponseEntity<String> response = restTemplate.getForEntity("/v1/admin/students/", String.class);
+        System.out.println(response.getBody());
+        Assertions.assertThat(response.getStatusCode().value()).isEqualTo(401);
+    }
+
+}
